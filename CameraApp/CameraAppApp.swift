70ccb50e8e042ac2fc6900d72cb8a2d565abe64d
author <author@gmail.com>
2023-08-22 12:15:14 +0800

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Elizabeth on 2023/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
